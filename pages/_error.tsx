a1a72550aa66923a1c819d272fd7b0c97a228f13
“Summit <summitchronicles17@gmail.com>
2025-08-28 09:40:50 +0530

    fix: type for CustomErrorComponent props

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,17 +1,12 @@
-import * as Sentry from "@sentry/nextjs";
+// pages/_error.tsx
 import Error from "next/error";
 
-const CustomErrorComponent = (props) => {
-  return <Error statusCode={props.statusCode} />;
-};
+interface ErrorProps {
+  statusCode: number;
+}
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
-  // In case this is running in a serverless function, await this in order to give Sentry
-  // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData);
-
-  // This will contain the status code of the response
-  return Error.getInitialProps(contextData);
+const CustomErrorComponent = ({ statusCode }: ErrorProps) => {
+  return <Error statusCode={statusCode} />;
 };
 
 export default CustomErrorComponent;
